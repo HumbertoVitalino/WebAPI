fcef09446bbd4c87ce8a1d4b66f20454d700c636
Humberto Vitalino <humbertodark@outlook.com>
2024-11-19 23:12:41 -0300

    feat: response model

--- Models/ResponseModel.cs
+++ Models/ResponseModel.cs
@@ -1,6 +1,8 @@
-﻿namespace WebAPI.Models
+﻿namespace WebAPI.Models;
+
+public class ResponseModel<T>
 {
-    public class ResponseModel
-    {
-    }
+    public T? Dados { get; set; }
+    public string Mensagem { get; set; } = string.Empty;
+    public bool Status { get; set; } = true;
 }
