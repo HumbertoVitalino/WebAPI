4bb7a0cbade823efce6ff2b86170c6601c5bf26e
Humberto Vitalino <humbertodark@outlook.com>
2024-11-19 23:24:24 -0300

    refactor: autor service

--- Services/Autor/AutorService.cs
+++ Services/Autor/AutorService.cs
@@ -1,5 +1,27 @@
-﻿namespace WebAPI.Services.Autor;
+﻿using WebAPI.Data;
+using WebAPI.Models;
+
+namespace WebAPI.Services.Autor;
 
 public class AutorService : IAutorInterface
 {
+    private readonly AppDbContext _context;
+    public AutorService(AppDbContext context)
+    {
+        _context = context;
+    }
+    public Task<ResponseModel<AutorModel>> BuscarAutorPorIdAutor(int idAutor)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseModel<AutorModel>> BuscarAutorPorIdLivro(int idLivro)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<ResponseModel<List<AutorModel>>> ListarAutores()
+    {
+        throw new NotImplementedException();
+    }
 }
