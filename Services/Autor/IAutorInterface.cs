a6e10248ed1a5fbac755242519940d5126868ed6
Humberto Vitalino <humbertodark@outlook.com>
2024-11-21 14:11:58 -0300

    refactor: interface

--- Services/Autor/IAutorInterface.cs
+++ Services/Autor/IAutorInterface.cs
@@ -1,4 +1,5 @@
-﻿using WebAPI.Models;
+﻿using WebAPI.Dto.Autor;
+using WebAPI.Models;
 
 namespace WebAPI.Services.Autor;
 
@@ -7,4 +8,7 @@ public interface IAutorInterface
     Task<ResponseModel<List<AutorModel>>> ListarAutores();
     Task<ResponseModel<AutorModel>> BuscarAutorPorIdAutor(int idAutor);
     Task<ResponseModel<AutorModel>> BuscarAutorPorIdLivro(int idLivro);
+    Task<ResponseModel<List<AutorModel>>> CriarAutor(AutorCriacaoDto autorCriacaoDto);
+    Task<ResponseModel<List<AutorModel>>> EditarAutor(AutorEdicaoDto autorEdicaoDto);
+    Task<ResponseModel<List<AutorModel>>> ExcluirAutor(int idAutor);
 }
