1e65038109c5e3d00a7538434a8d152a2807b040
Humberto Vitalino <humbertodark@outlook.com>
2024-11-22 17:33:57 -0300

    refactor: IlivroInterface

--- Services/Livros/ILivroInterface.cs
+++ Services/Livros/ILivroInterface.cs
@@ -6,7 +6,7 @@ namespace WebAPI.Services.Livros;
 public interface ILivroInterface
 {
     Task<ResponseModel<List<LivroModel>>> ListarLivros();
-    Task<ResponseModel<LivroModel>> ListarLivrosPorAutor(int idAutor);
+    Task<ResponseModel<List<LivroModel>>> ListarLivrosPorAutor(int idAutor);
     Task<ResponseModel<LivroModel>> BuscarLivroPorId(int idLivro);
     Task<ResponseModel<List<LivroModel>>> CriarLivro(LivroCriacaoDto livroCriacaoDto);
     Task<ResponseModel<List<LivroModel>>> EditarLivro(LivroEdicaoDto livroEdicaoDto);
