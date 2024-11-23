57928eebb90bd4edb82459eb8e96f3da06a39eb2
Humberto Vitalino <humbertodark@outlook.com>
2024-11-23 08:36:37 -0300

    auto update

--- obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbbe8c2af19fdb941839eda5344e3c20a85aea57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2d8650f3b6df4ccca1d9a6cdd8d7908a6122a46")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
