d44236d061362c96585890b824eb0961c7c09107
Humberto Vitalino <humbertodark@outlook.com>
2024-11-23 08:37:17 -0300

    refactor: to autorvinculo

--- Dto/Livro/LivroCriacaoDto.cs
+++ Dto/Livro/LivroCriacaoDto.cs
@@ -1,9 +1,10 @@
-﻿using WebAPI.Models;
+﻿using WebAPI.Dto.Vinculo;
+using WebAPI.Models;
 
 namespace WebAPI.Dto.Livro;
 
 public class LivroCriacaoDto
 {
     public string Titulo { get; set; }
-    public AutorModel Autor { get; set; }
+    public AutorVinculoDto Autor { get; set; }
 }
